4292ecd58edfa771409419ca0fb61d4987a42138
Kevin Hester <kevinh@geeksville.com>
2014-03-06 09:55:13 -0800

    send a valid mavlink packet in test case

--- src/main/java/com/geeksville/apiproxy/TestClient.java
+++ src/main/java/com/geeksville/apiproxy/TestClient.java
@@ -14,7 +14,14 @@ public class TestClient {
 			int interfaceNum = 0;
 			int sysId = 1;
 			webapi.setVehicleId("blahblahvehicle", interfaceNum, sysId);
-			webapi.filterMavlink(interfaceNum, new byte[] { 1, 2, 3, 4, 5 });
+
+			byte[] payload = new byte[] { (byte) 0xfe, (byte) 0x0e,
+					(byte) 0x9d, (byte) 0x01, (byte) 0x01, (byte) 0x1d,
+					(byte) 0xf9, (byte) 0x46, (byte) 0x01, (byte) 0x00,
+					(byte) 0x33, (byte) 0x03, (byte) 0x7c, (byte) 0x44,
+					(byte) 0xec, (byte) 0x51, (byte) 0x1e, (byte) 0xbe,
+					(byte) 0x27, (byte) 0x01, (byte) 0xca, (byte) 0x8f };
+			webapi.filterMavlink(interfaceNum, payload);
 			webapi.flush();
 
 			System.out.println("Test successful");
