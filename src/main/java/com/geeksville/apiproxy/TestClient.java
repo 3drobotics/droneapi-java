6d6b47363e7100b34d78148e1d83877b895f8889
Kevin Hester <kevinh@geeksville.com>
2014-05-02 14:43:27 -1000

    droidplanner doesn't enforce framing - simulate that in the test case

--- src/main/java/com/geeksville/apiproxy/TestClient.java
+++ src/main/java/com/geeksville/apiproxy/TestClient.java
@@ -53,13 +53,15 @@ public class TestClient extends GCSHookImpl {
 	public static void runTest() throws UnknownHostException, IOException {
 		TestClient webapi = new TestClient();
 		webapi.connect();
-		byte[] payload = new byte[] { (byte) 0xfe, (byte) 0x0e, (byte) 0x9d,
+		// Test splitting packet into two calls - to show bug in server
+		byte[] payload1 = new byte[] { (byte) 0xfe, (byte) 0x0e, (byte) 0x9d,
 				(byte) 0x01, (byte) 0x01, (byte) 0x1d, (byte) 0xf9,
-				(byte) 0x46, (byte) 0x01, (byte) 0x00, (byte) 0x33,
-				(byte) 0x03, (byte) 0x7c, (byte) 0x44, (byte) 0xec,
-				(byte) 0x51, (byte) 0x1e, (byte) 0xbe, (byte) 0x27,
-				(byte) 0x01, (byte) 0xca, (byte) 0x8f };
-		webapi.filterMavlink(webapi.interfaceNum, payload);
+				(byte) 0x46, (byte) 0x01, (byte) 0x00, (byte) 0x33 };
+		byte[] payload2 = new byte[] { (byte) 0x03, (byte) 0x7c, (byte) 0x44,
+				(byte) 0xec, (byte) 0x51, (byte) 0x1e, (byte) 0xbe,
+				(byte) 0x27, (byte) 0x01, (byte) 0xca, (byte) 0x8f };
+		webapi.filterMavlink(webapi.interfaceNum, payload1);
+		webapi.filterMavlink(webapi.interfaceNum, payload2);
 		webapi.close();
 	}
 
